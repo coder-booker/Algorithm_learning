50dabaeebaecca1c6364d071a02ffd4fd7452aaa
coder-booker <cp793604890@gmail.com>
2024-06-03 13:17:57 +0800

    Finished 74.cpp using binary search

--- leetcode/matrix/74.cpp
+++ leetcode/matrix/74.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m, n;
-        m = matrix.size();
-        n = matrix[0].size();
-        // for ( int i = 0; i < m; ++i ) {
-            
+        // // Method 1: O(m+n)
+        // int m, n;
+        // m = matrix.size();
+        // n = matrix[0].size();
+        // int i = 0;
+        // while ( i < m && target >= matrix[i][0] ) {
+        //     ++i;
         // }
-        int i = 0;
-        while ( i < m && target >= matrix[i][0] ) {
-            ++i;
-            // cout << i << '\n';
-        }
-        if ( i > 0 ) {
-            i -= 1;
-        }
-        for ( int j = 0; j < n; ++j ) {
-            if ( matrix[i][j] == target ) {
-                return 1;
+        // if ( i > 0 ) {
+        //     i -= 1;
+        // }
+        // for ( int j = 0; j < n; ++j ) {
+        //     if ( matrix[i][j] == target ) {
+        //         return 1;
+        //     }
+        // }
+        // return 0;
+
+        // Method 2: BInary search O(log(mn)) = O(log(m)+log(n))
+        int m, n, left, right, mid, temp;
+        m = matrix.size();      // col
+        n = matrix[0].size();   // row
+        left = 0;
+        right = m*n - 1;
+
+        while ( left <= right ) {
+            mid = left + (right - left) / 2;
+            temp = matrix[mid/n][mid%n];
+            if ( temp == target ) {
+                return true;
+            } else if ( temp < target ) {
+                left = mid + 1;
+            } else if ( temp > target ) {
+                right = mid - 1;
             }
         }
-        return 0;
+        return false;
     }
 };

37094442d31e5a25ccf22acdec12c07eccda6744
coder-booker <cp793604890@gmail.com>
2024-06-06 09:45:25 +0800

    Reviewed 322.cpp and 25.cpp

--- leetcode/dp/322.cpp
+++ leetcode/dp/322.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // unordered_map<int, int> dp;
         if ( amount == 0 ) {
             return 0;
         }
         vector<int> dp(amount+1, amount+1);
-        dp[0] = 0;
+        dp[0] = 0;       // so the edge case "i-coins[j]==0" can be handle
         for ( int i = 0; i < dp.size(); ++i ) {
             for ( int j = 0; j < coins.size(); ++j ) {
-                if ( i - coins[j] < 0 ) {
-                    continue;
+                if ( i - coins[j] >= 0 ) {
+                    dp[i] = min(dp[i], dp[i-coins[j]] + 1);
                 }
-                dp[i] = min(dp[i], dp[i-coins[j]] + 1);
             }
         }
         return dp[amount] != amount+1 ? dp[amount] : -1;
+
+        // if ( amount == 0 ) {
+        //     return 0;
+        // }
+        // vector<int> dp(amount, amount+1);
+        // // dp[0] = 0;
+        // for ( int i = 0; i < dp.size(); ++i ) {
+        //     for ( int coin : coins ) {
+        //         if ( i+1 - coin > 0 ) {
+        //             dp[i] = min(dp[i], dp[i-coin] + 1);
+        //         } else if ( i+1 - coin == 0 ) {
+        //             dp[i] = 1;
+        //         }
+        //     }
+        // }
+        // return dp[amount-1] != amount+1 ? dp[amount-1] : -1;
     }
 };
--- leetcode/linked_list/25.cpp
+++ leetcode/linked_list/25.cpp
@@ -11,80 +11,81 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        // // Method 1: iteration
-        // if ( k <= 1 || !head || !(head->next) ) {
-        //     return head;
-        // }
-        // ListNode *traverse_ptr = new ListNode(0, head);         // for proper reverse logic (see the loop below)
-        // ListNode *rev_head, *rev_head_prev;
-        // int rev_progress = 0;
-        // ListNode *dummy_head = new ListNode(0, traverse_ptr);   // for return value
-
-        // while ( traverse_ptr ) {
-        //     if ( rev_progress == 0 ) {      // record a ptr for later reverse when k elements are traversed
-        //         rev_head_prev = traverse_ptr;
-        //     }
-        //     if ( rev_progress == k ) {
-        //         traverse_ptr = partialReverse(rev_head_prev, k); // return the head of reversed partial linked list
-        //         rev_progress = 0;
-        //     } else {
-        //         traverse_ptr = traverse_ptr->next;
-        //         ++rev_progress;
-        //     }
-        // }
-        // return dummy_head->next->next;
-
-
-        // Method 2: recursion
+        // Method 1: iteration
         if ( k <= 1 || !head || !(head->next) ) {
             return head;
         }
-        return recurReverse(head, k);
-    }
+        ListNode *traverse_ptr = new ListNode(0, head);         // for proper reverse logic (see the loop below)
+        ListNode *rev_head, *rev_head_prev;
+        int rev_progress = 0;
+        ListNode *dummy_head = traverse_ptr;   // for return value
 
-    // // For Method 1
-    // ListNode* partialReverse(ListNode* head, int n) {
-    //     ListNode *i = head->next, *j = head->next->next;
-    //     ListNode *k = j->next;
-    //     ListNode *tail = i;
-    //     for ( int p = 0; p < n-1; ++p ) {
-    //         j->next = i;
-    //         i = j;
-    //         j = k;
-    //         if ( k ) {
-    //             k = k->next;
-    //         }
-    //     }
-    //     head->next = i;   // concatenate the rev_head back to previous ptr
-    //     tail->next = j;   // concatenate the tail to next ptr;
-    //     return tail;
-    // }
-
-    // For Method 2
-    ListNode* recurReverse(ListNode* head, int k) {
-        ListNode* p = head;
-        for ( int i = 0; i < k; ++i ) {
-            if ( !p ) {
-                return head;
+        while ( traverse_ptr ) {
+            if ( rev_progress == 0 ) {      // record a ptr for later reverse when k elements are traversed
+                rev_head_prev = traverse_ptr;
             }
-            p = p->next;
+            if ( rev_progress == k ) {
+                traverse_ptr = partialReverse(rev_head_prev, k); // return the head of reversed partial linked list
+                rev_progress = 0;
+            } else {
+                traverse_ptr = traverse_ptr->next;
+                ++rev_progress;
+            }
         }
-        ListNode* tail;
-        ListNode* hair = new ListNode(0, head);
-        pair<ListNode*, ListNode*> result = subRecurReverse(head, k);
-        head = result.first;
-        tail = result.second;
-        tail->next = recurReverse(p, k);
-        return head;
+        return dummy_head->next;
+
+
+        // // Method 2: recursion
+        // if ( k <= 1 || !head || !(head->next) ) {
+        //     return head;
+        // }
+        // return recurReverse(head, k);
     }
-    pair<ListNode*, ListNode*> subRecurReverse(ListNode* head, int n) {
-        if ( n == 1 ) {
-            return {head, head};
+
+    // For Method 1
+    ListNode* partialReverse(ListNode* head, int n) {
+        ListNode *i = head->next, *j = head->next->next;
+        ListNode *k = j->next;
+        ListNode *tail = i;
+        for ( int p = 0; p < n-1; ++p ) {
+            j->next = i;
+            i = j;
+            j = k;
+            if ( k ) {
+                k = k->next;
+            }
         }
-        pair<ListNode*, ListNode*> result = subRecurReverse(head->next, n-1);
-        ListNode *last = result.first;
-        head->next->next = head;
-        head->next = NULL;
-        return {last, head};
+        // i here is the rev_head, j is next ptr of the entire rev_list in the original list
+        head->next = i;   // concatenate the rev_head back to previous ptr
+        tail->next = j;   // concatenate the tail to next ptr;
+        return tail;
     }
+
+    // // For Method 2
+    // ListNode* recurReverse(ListNode* head, int k) {
+    //     ListNode* p = head;
+    //     for ( int i = 0; i < k; ++i ) {
+    //         if ( !p ) {
+    //             return head;
+    //         }
+    //         p = p->next;
+    //     }
+    //     ListNode* tail;
+    //     ListNode* hair = new ListNode(0, head);
+    //     pair<ListNode*, ListNode*> result = subRecurReverse(head, k);
+    //     head = result.first;
+    //     tail = result.second;
+    //     tail->next = recurReverse(p, k);
+    //     return head;
+    // }
+    // pair<ListNode*, ListNode*> subRecurReverse(ListNode* head, int n) {
+    //     if ( n == 1 ) {
+    //         return {head, head};
+    //     }
+    //     pair<ListNode*, ListNode*> result = subRecurReverse(head->next, n-1);
+    //     ListNode *last = result.first;
+    //     head->next->next = head;
+    //     head->next = NULL;
+    //     return {last, head};
+    // }
 };

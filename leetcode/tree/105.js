3e4ff8bc61471cfc86ae380d0fa1ec4739110454
coder-booker <54313009+coder-booker@users.noreply.github.com>
2024-07-16 17:37:38 +0800

    Finished 105.js and learnt how to build tree from traversal result
    However indexing problem is not clear yet.

---
+++ leetcode/tree/105.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+  if ( preorder.length === 0 ) {
+      return new TreeNode(null);
+  }
+  if ( preorder.length === 1 ) {
+      return new TreeNode(preorder[0]);
+  }
+  function recurBuild(preS, preE, inS, inE) {
+      // console.log(preS, preE, inS, inE);
+      if ( preS > preE ) {
+          return null;
+      }
+
+      let root = new TreeNode(preorder[preS]);
+      const inRoot = inorder.indexOf(root.val);
+      const leftTreeSize = inRoot - inS;
+      // const rightTreeSize = inE - inRoot;
+
+      root.left = recurBuild(preS+1, preS+leftTreeSize, inS, inRoot-1);
+      root.right = recurBuild(preS+leftTreeSize+1, preE, inRoot+1, inE);
+
+      return root;
+  }
+  return recurBuild(0, preorder.length-1, 0, inorder.length-1);
+};
